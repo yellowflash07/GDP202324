db6ef72e1e8915e1dcac934603e3e4ce954bd22a
michaelfeeney <michaelwcfeeney@gmail.com>
2023-09-07 15:13:37 -0400

    A triangle of death!

--- CODE/OpenGLIsMeh/Project1/theMain.cpp
+++ CODE/OpenGLIsMeh/Project1/theMain.cpp
@@ -1,10 +1,18 @@
 //https://www.glfw.org/docs/latest/quick.html
 
-#include <glad/gl.h>
+#include <glad/glad.h>
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
+#include <iostream>
 
-#include "linmath.h"
+//#include "linmath.h"
+#include <glm/glm.hpp>
+#include <glm/vec3.hpp> // glm::vec3
+#include <glm/vec4.hpp> // glm::vec4
+#include <glm/mat4x4.hpp> // glm::mat4
+#include <glm/gtc/matrix_transform.hpp> 
+// glm::translate, glm::rotate, glm::scale, glm::perspective
+#include <glm/gtc/type_ptr.hpp> // glm::value_ptr
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -53,6 +61,8 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 
 int main(void)
 {
+    std::cout << "About to blow you mind with OpenGL!" << std::endl;
+
     GLFWwindow* window;
     GLuint vertex_buffer, vertex_shader, fragment_shader, program;
     GLint mvp_location, vpos_location, vcol_location;
@@ -75,7 +85,7 @@ int main(void)
     glfwSetKeyCallback(window, key_callback);
 
     glfwMakeContextCurrent(window);
-    gladLoadGL(glfwGetProcAddress);
+    gladLoadGLLoader( (GLADloadproc)glfwGetProcAddress);
     glfwSwapInterval(1);
 
     // NOTE: OpenGL error checks have been omitted for brevity
@@ -112,7 +122,8 @@ int main(void)
     {
         float ratio;
         int width, height;
-        mat4x4 m, p, mvp;
+//        mat4x4 m, p, mvp;
+        glm::mat4 m, p, v, mvp;
 
         glfwGetFramebufferSize(window, &width, &height);
         ratio = width / (float)height;
@@ -120,13 +131,37 @@ int main(void)
         glViewport(0, 0, width, height);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        mat4x4_identity(m);
-        mat4x4_rotate_Z(m, m, (float)glfwGetTime());
-        mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
-        mat4x4_mul(mvp, p, m);
+        //         mat4x4_identity(m);
+        m = glm::mat4(1.0f);
+
+        //mat4x4_rotate_Z(m, m, (float) glfwGetTime());
+        glm::mat4 rotateZ = glm::rotate(glm::mat4(1.0f),
+                                        (float)glfwGetTime(),
+                                        glm::vec3(0.0f, 0.0, 1.0f));
+
+        //mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+        p = glm::perspective(0.6f,
+                             ratio,
+                             0.1f,
+                             1000.0f);
+
+        v = glm::mat4(1.0f);
+
+        glm::vec3 cameraEye = glm::vec3(0.0, 0.0, -4.0f);
+        glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+        glm::vec3 upVector = glm::vec3(0.0f, 1.0f, 0.0f);
+
+        v = glm::lookAt(cameraEye,
+                        cameraTarget,
+                        upVector);
+
+       //mat4x4_mul(mvp, p, m);
+        mvp = p * v * m;
 
         glUseProgram(program);
-        glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*)mvp);
+        //glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*) mvp);
+        glUniformMatrix4fv(mvp_location, 1, GL_FALSE, glm::value_ptr(mvp));
+
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
